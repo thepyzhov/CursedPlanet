fa4c961471c52aba80a78b7b90fb08d1a638f1a6
Dmitry Pyzhov <fiche40@gmail.com>
2020-08-28 18:11:02 +0300

    Base implementation of player interactions
    - Med-Station interaction area

---
+++ KenneyGameJamProject/Assets/Scripts/MSInteractionArea.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MSInteractionArea : MonoBehaviour
+{
+    public int medGelAmount = 100;
+
+    SpriteRenderer spriteRenderer;
+    Color baseColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        baseColor = spriteRenderer.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            spriteRenderer.color = Color.green;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            spriteRenderer.color = baseColor;
+        }
+    }
+}
---
+++ KenneyGameJamProject/Assets/Scripts/SessionManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionManager : MonoBehaviour
+{
+    int medGelAmount;
+
+	private void Awake() {
+		medGelAmount = 0;
+	}
+
+	void GetMedGel() {
+
+	}
+
+	void SpendMedGel() {
+
+	}
+}
